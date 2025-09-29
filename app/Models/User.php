a9a50634c927e1ac58a2ad3bc9b4400c7fd0cd85
Muhammad Dausyaf Aryandha <muhammaddausyaf@gmail.com>
2025-09-29 07:09:04 +0700

    update livewire project

--- app/Models/User.php
+++ app/Models/User.php
@@ -22,6 +22,11 @@ class User extends Authenticatable
         'password',
     ];
 
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
+
     /**
      * The attributes that should be hidden for serialization.
      *
